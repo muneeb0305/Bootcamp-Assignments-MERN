3bc765e15ac4637a52a32621504456aab774e696
Muneeb Ahmed <muneebahmed0305@gmail.com>
2023-04-06 12:39:31 +0500

    Assignment 3 completed

---
+++ Assignment 3/script.js
@@ -0,0 +1,31 @@
+function calculation() {
+  let fNum = document.getElementsByName("num1")[0].value;
+  let SNum = document.getElementsByName("num2")[0].value;
+  let Operator = document.getElementsByName("operator")[0].value;
+  let result;
+  switch (Operator) {
+    case "+":
+      result = parseInt(fNum) + parseInt(SNum);
+      break;
+
+    case "-":
+      result = parseInt(fNum) - parseInt(SNum);
+      break;
+
+    case "/":
+      result = parseInt(fNum) / parseInt(SNum);
+      break;
+
+    case "*":
+      result = parseInt(fNum) * parseInt(SNum);
+      break;
+
+    default:
+      alert("This Symbol is Not Existed");
+      break;
+  }
+  let Total = document.getElementById("total");
+  let element = document.getElementById("heading");
+  element.style.visibility = "visible";
+  Total.innerHTML = result;
+}
