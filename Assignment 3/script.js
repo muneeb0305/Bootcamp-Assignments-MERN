b45c52675334360c6cde44a43d6d195d560857f4
Muneeb Ahmed <muneebahmed0305@gmail.com>
2023-04-07 12:34:41 +0500

    Assignment 3

--- Assignment 3/script.js
+++ Assignment 3/script.js
@@ -1,32 +1,10 @@
-function calculation() {
-  let fNum = document.getElementsByName("num1")[0].value;
-  let SNum = document.getElementsByName("num2")[0].value;
- let Operator= document.getElementById("+").innerHTML
- console.log(Operator)
-  let result;
-  switch (Operator) {
-    case "+":
-      result = parseInt(fNum) + parseInt(SNum);
-      break;
-
-    case "-":
-      result = parseInt(fNum) - parseInt(SNum);
-      break;
-
-    case "/":
-      result = parseInt(fNum) / parseInt(SNum);
-      break;
-
-    case "*":
-      result = parseInt(fNum) * parseInt(SNum);
-      break;
-
-    default:
-      alert("This Symbol is Not Existed");
-      break;
+let insert = (num) => {
+  document.getElementsByName("textview")[0].value =
+    document.getElementsByName("textview")[0].value + num;
+};
+let equal = () => {
+  let expression = document.getElementsByName("textview")[0].value;
+  if (expression) {
+    document.getElementsByName("textview")[0].value = eval(expression);
   }
-  let Total = document.getElementById("total");
-  let element = document.getElementById("heading");
-  element.style.visibility = "visible";
-  Total.innerHTML = result;
-}
+};
