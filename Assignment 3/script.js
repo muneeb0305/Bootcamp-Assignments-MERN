c785ce6b17b73b02beb32ef838ba78f4e2924eeb
Muneeb Ahmed <muneebahmed0305@gmail.com>
2023-04-10 13:57:05 +0500

    Update

--- Assignment 3/script.js
+++ Assignment 3/script.js
@@ -1,6 +1,19 @@
 let insert = (num) => {
-  document.getElementsByName("textview")[0].value =
-    document.getElementsByName("textview")[0].value + num;
+  let expression = document.getElementsByName("textview")[0].value;
+  // let expression2 = document.getElementsByName("textview")[0].value;
+  let str = expression.length-1 ;
+  console.log(str)
+  // let str2 = expression2.length;
+  // console.log(str2)
+  if (
+    expression[str] !== "-" &&
+    expression[str] !== "+" &&
+    expression[str] !== "*" &&
+    expression[str] !== "/" ||
+    typeof num === "number"
+  ) {
+    document.getElementsByName("textview")[0].value += num;
+  }
 };
 let equal = () => {
   let expression = document.getElementsByName("textview")[0].value;
